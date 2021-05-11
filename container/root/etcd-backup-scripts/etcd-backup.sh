bf50360d2f74d623b876137101195b54d33bfa41
edubois10 <59562718+edubois10@users.noreply.github.com>
2021-05-11 14:43:16 +0200

    Update etcd-backup.sh

--- container/root/etcd-backup-scripts/etcd-backup.sh
+++ container/root/etcd-backup-scripts/etcd-backup.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 DATE=$(date +%Y%m%dT%H%M%S)
-    
-read VAULT_SERVER < /etcd-backup-scripts/etcd-backup-vault-server/vault-server
-read VAULT_TOKEN < /etcd-backup-scripts/etcd-backup-vault-key/etcd-backup-secret
-read AZURE_STORAGE_URL < /etcd-backup-scripts/etcd-backup-azure-storage-url/azure-url
-
-VAULT_KEY=$(curl -k -H "X-Vault-Token: $VAULT_TOKEN" -X GET $VAULT_SERVER | jq -r '.data.data.encryption')
-AZURE_STORAGE_TOKEN=$(curl -k -H "X-Vault-Token: $VAULT_TOKEN" -X GET $VAULT_SERVER | jq -r '.data.data.azureKey')  
 
 echo "STARTING THE ETCD BACKUP SCRIPT"
 
@@ -22,18 +15,13 @@ if [ $? -eq 0 ]; then
     ls -l
     echo "INFO" "Compressing etcd snapshot..."
     export XZ_OPT=-5
-    tar -cvJf "etcd-backup-${DATE}.tar.xz" "backup"
-
-    echo "INFO" "Encrypting compressed etcd snapshot using generated symmetric key..."
-    openssl enc -aes-256-cbc -salt -in "etcd-backup-${DATE}.tar.xz" -out "etcd-backup-${DATE}.tar.xz.enc" -pass "pass:${VAULT_KEY}"
     
     echo "Creating backupfile"
-    mkdir /etcd-backup-az
-    cp -r /assets/etcd-backup-${DATE}.tar.xz.enc /etcd-backup-az/
-    ls /etcd-backup-az
+    mkdir /etcd-backup
+    cp -r /assets/etcd-backup-${DATE}.tar.xz.enc /etcd-backup/
+    ls /etcd-backup
     #rm -r /backup/assets/*
-    echo "Coping backup file into Azure Storage Blob"
-    azcopy copy "/etcd-backup-az/*" "${AZURE_STORAGE_URL}/${AZURE_STORAGE_TOKEN}" --recursive
+  
     exit 0
 fi
 echo "Backup attempts failed. Please FIX !!!"
